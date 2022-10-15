c3c4d687f74cc91e2784f353862ebc401bd28393
Bryce Watt <brycewatt@gmail.com>
2022-10-15 17:40:25 -0500

    Initial attempt at google OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   resources :member_orgs
   resources :current_members
   root 'current_members#index'
+  get 'auth/:provider/callback' => 'sessions#omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# # Routes for Google authentication
+# get 'auth/:provider/callback' => 'sessions#omniauth'
+# get 'auth/failure', to: redirect('/')
--- db/migrate/20220921171405_create_current_members.rb
+++ db/migrate/20220921171405_create_current_members.rb
@@ -1,7 +1,7 @@
 class CreateCurrentMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :current_members do |t|
-      t.integer :memberID
+      t.string :memberID
       t.string :name
       t.string :email
       t.boolean :isAdmin
@@ -10,4 +10,9 @@ class CreateCurrentMembers < ActiveRecord::Migration[6.1]
       t.timestamps
     end
   end
+end
+# ActiveRecord::Migration
+def change
+  add_column :users, google_token, :string
+  add_column :users, google_refresh_token, :string
 end

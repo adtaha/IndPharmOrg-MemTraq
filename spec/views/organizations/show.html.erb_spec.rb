9ecd0f977392996ecd18d6ed1386f5e6a6d1948d
jaechizzle4 <89861095+jaechizzle4@users.noreply.github.com>
2022-10-17 06:34:04 -0500

    update to spec

--- spec/views/organizations/show.html.erb_spec.rb
+++ spec/views/organizations/show.html.erb_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe "organizations/show", type: :view do
   before(:each) do
     @organization = assign(:organization, Organization.create!(
       organizationID: 2,
-      name: "Name",
-      email: "Email"
+      organizationName: "Organization Name",
+      organizationEmail: "Organization Email"
     ))
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/2/)
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/Email/)
+    expect(rendered).to match(/Organization Name/)
+    expect(rendered).to match(/Organization Email/)
   end
 end

ee9f67efbac426b96a4ec0c2c0fe91af607a538b
root <root@localhost>
2022-10-19 19:36:15 +0000

    Got rid of unecessary comments

--- db/migrate/20220921171405_create_current_members.rb
+++ db/migrate/20220921171405_create_current_members.rb
@@ -14,8 +14,3 @@ class CreateCurrentMembers < ActiveRecord::Migration[6.1]
     end
   end
 end
-# # ActiveRecord::Migration
-# def change
-#   add_column :users, google_token, :string
-#   add_column :users, google_refresh_token, :string
-# end

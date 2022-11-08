7ba26d85b2cbcb157543d5b179e0449eb352fdd9
Bryce Watt <brycewatt@gmail.com>
2022-11-07 22:31:02 -0600

    trying to undo stupid git stuff

--- app/models/current_member.rb
+++ app/models/current_member.rb
@@ -3,16 +3,4 @@ class CurrentMember < ApplicationRecord
 
      validates :memberID, :name, :email, presence: true
      validates :isAdmin, :isAlumni, inclusion: { in: [true, false] }
-
-
-
-     # def self.from_omniauth(response)
-     #      CurrentMember.find_or_create_by(memberID: response[:uid]) do |u|
-     #           u.name = response[:info][:name]
-     #           u.email = response[:info][:email]
-     #           u.token = response[:info][:token]
-     #           u.refresh_token = response[:info][:refresh_token]
-     #           u.oauth_expires_at = response[:info][:oauth_expires_at]
-     #      end
-     # end
 end

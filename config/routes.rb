d961455b84a67a81e0fe67bd91af80ec0688389e
Bryce Watt <brycewatt@gmail.com>
2022-11-10 17:14:20 -0600

    added homepage and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'current_members#index'
+  root to: 'dashboards#home'
   devise_for :current_members, controllers: { omniauth_callbacks: 'current_members/omniauth_callbacks' }
   devise_scope :current_member do
     get 'current_members/sign_in', to: 'current_members/sessions#new', as: :new_current_member_session
     get 'current_members/sign_out', to: 'current_members/sessions#destroy', as: :destroy_current_member_session
   end
+  get 'home', to: 'dashboards#home'
   resources :speakers
   resources :organizations
   resources :member_orgs

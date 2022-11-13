8c28cf6e38a4f435d14ccaa5139fba9703caa707
dkrauss1999 <dkrauss1999@gmail.com>
2022-11-13 14:50:47 -0600

    Jotform Page
    creation of jotform page with jotform embedded into it

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :speakers
-  resources :speakers
   resources :organizations
   resources :member_orgs
   resources :current_members
   root 'current_members#index'
+  get '/jotform' => 'jotform#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get 'auth/:provider/callback' => 'sessions#omniauth'

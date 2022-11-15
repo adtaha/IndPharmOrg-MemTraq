65984450c900204e1043db1a2ec8418f9c4f53b7
Bryce Watt <brycewatt@gmail.com>
2022-11-14 19:59:05 -0600

    Only tamu emails can login now

--- app/models/current_member.rb
+++ app/models/current_member.rb
@@ -7,7 +7,7 @@ class CurrentMember < ApplicationRecord
      # validates :memberID, :name, :email, presence: true
      # validates :isAdmin, :isAlumni, inclusion: { in: [true, false] }
      def self.from_google(email:, name:, uid:, avatar_url:)
-          return nil unless email =~ /@gmail.com || @tamu.edu\z/
+          return nil unless email =~ /@tamu.edu\z/
           create_with(uid: uid, name: name, avatar_url: avatar_url).find_or_create_by!(email: email)
      end
 end
